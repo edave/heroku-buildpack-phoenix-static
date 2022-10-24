e37e5c504d9464d18aab2cf06d0fd83684b3427c
David Pitman <dave@convergeindustries.com>
2022-10-23 21:33:31 -0700

    Removing --check-files for Yarn 3

--- lib/build.sh
+++ lib/build.sh
@@ -152,7 +152,7 @@ install_npm_deps() {
 }
 
 install_yarn_deps() {
-  yarn install --check-files --cache-folder $cache_dir/yarn-cache --pure-lockfile 2>&1
+  yarn install --cache-folder $cache_dir/yarn-cache --pure-lockfile 2>&1
 }
 
 install_bower_deps() {
